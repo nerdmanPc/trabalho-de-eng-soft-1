e9bc09133c9fa72ee3a00afdf87b8766bf351d75
Baquara <Baquara@users.noreply.github.com>
2017-09-02 14:41:57 -0300

    Adicionado código nos métodos

--- Trabalho_de_EngSoftware_Felipe_Abreu_&_Pedro_Costa/Biblioteca.java
+++ Trabalho_de_EngSoftware_Felipe_Abreu_&_Pedro_Costa/Biblioteca.java
@@ -26,12 +26,66 @@ public class Biblioteca{
     private Biblioteca(){ 
       
     }
-    public void fazeEmprestimo(int id_usuario, int id_livro){}
-    public void devolverLivro(int id_usuario, int id_livro){}
+    public void novoLivro(int codigo,String titulo,String editora,String[] autores,int edicao, int ano_de_publicacao){
+        Livro liv = new Livro(codigo,titulo,editora,autores,edicao,ano_de_publicacao);
+        livros.add(liv);
+    
+    }
+    
+        public void novoUsuario(int codigo, String nome,int diasdeemprestimo, int maximodelivros){
+        Usuario usr = new Usuario(codigo,nome,diasdeemprestimo,maximodelivros);
+        usuarios.add(usr);
+    
+    }
+    
+    public void fazerEmprestimo(int id_usuario, int id_livro){
+    
+      for (Livro liv : livros) {
+            if(liv.getCodigo()==id_livro){
+                if(liv.getDisponibilidade()==true){
+            liv.setDisponibilidade(false);
+            liv.setUsuarioEmp(id_usuario);
+        }
+            }
+        }
+    
+    
+    
+    }
+    public void devolverLivro(int id_usuario, int id_livro){
+      for (Livro liv : livros) {
+            if(liv.getCodigo()==id_livro){
+                if(liv.getUsuarioEmp()==id_usuario){
+                if(liv.getDisponibilidade()==false){
+            liv.setDisponibilidade(true);
+            liv.setUsuarioEmp(0);
+        }
+            }
+        }
+        }
+    
+    }
     public void reservarLivro(int id_usuario, int id_livro){}
     public void assinarLivro(int id_professor, int id_livro){}
-    public void consultarLivro(int id_livro){}
-    public void consultarUsuario(int id_usuario){}
+    public void consultarLivro(int id_livro){
+       for (Livro liv : livros) {
+            if(liv.getCodigo()==id_livro){
+            System.out.println(liv.getTitulo() + " "+ liv.getEditora() + " " 
+            + liv.getAutores()+ " " + liv.getEdicao() +" " + liv.getano_de_publicacao());
+            
+            }
+        }
+    
+    }
+    public void consultarUsuario(int id_usuario){
+            for (Usuario user : usuarios) {
+            if(user.getCodigo()==id_usuario){
+            System.out.println(user.getNome() + " "+ user.getCodigo() + " " + user.getDiasdeemprestimo());
+            
+            }
+        }
+    
+    }
     public void consultarAssinatura(int id_livro){}
     
 }

e7ed965e4279593c152af6630411ebc11dc4bee9
nerdmanPc <nerdman_96@hotmail.com>
2017-09-03 05:22:27 -0300

    commit da madrugada

--- Trabalho_de_EngSoftware_Felipe_Abreu_&_Pedro_Costa/Reserva.java
+++ Trabalho_de_EngSoftware_Felipe_Abreu_&_Pedro_Costa/Reserva.java
@@ -1,9 +1,18 @@
+import java.util.GregorianCalendar;
+import java.text.SimpleDateFormat;
+
 public class Reserva
 {
+    private Livro livro;
+    private GregorianCalendar data_requisicao;
     
-    public Reserva(){
-        
+    public Reserva(Livro livro){
+        this.livro = livro;
+        this.data_requisicao = new GregorianCalendar();
     }
-
     
+    public String consulta(){
+        SimpleDateFormat date_format = new SimpleDateFormat("dd/MM/yyyy");
+        return livro.getTitulo() + " - " + date_format.format +"\n";
+    }
 }
